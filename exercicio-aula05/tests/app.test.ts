9a52991f94f8b893d0bac701bd5e7ff913712894
gabriela <gabrielamontanhini@gmail.com>
2023-09-28 17:50:01 -0300

    Exercicio aula 05

--- exercicio-aula05/tests/app.test.ts
+++ exercicio-aula05/tests/app.test.ts
@@ -21,6 +21,11 @@ describe("/fibonacci",() => {
   it("should return status 400 when elements param is not present ", async()=> {
     const response = await api.get("/fibonacci");
     expect(response.status).toBe(400)
-
+  })
+  it("should return an array and status 200 when parameter is valid number", async()=> {
+    const {status, body} = await api.get("/fibonacci?elements=10")
+    expect(status).toBe(200)
+    expect(body).toHaveLength(10)
+    expect(body).toEqual([ 0 , 1 , 1 , 2 , 3 , 5 , 8 , 13, 21, 34])
   })
 })
