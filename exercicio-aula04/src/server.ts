e9884c299941ab10c83ddb6d349c5e6d025191ce
gabriela <gabrielamontanhini@gmail.com>
2023-10-05 11:17:37 -0300

    Fix: correção do número de aula

--- exercicio-aula04/src/server.ts
+++
@@ -1,7 +0,0 @@
-import app from "./app";
-
-
-const port = 6000;
-app.listen(port, () => {
-  console.log(`Server is up and running on port ${port}`);
-})
--- exercicio-aula10/tests/app.test.ts
+++ exercicio-aula10/tests/app.test.ts
@@ -1,6 +1,6 @@
 import supertest from "supertest";
 
-import app from "./../src/app";
+import app from "../src/app";
 import prisma from "../src/database";
 import { AccessInput } from "../src/repository";
 
